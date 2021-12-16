e5d11833000cd3f46bae4585188c0c8d4920352e
21laforgue <90769817+21laforgue@users.noreply.github.com>
2021-12-16 11:39:28 +0100

    Update compteur.py

--- compteur.py
+++ compteur.py
@@ -1,7 +1,9 @@
 def compte():
     r"""Compte de 1 à 100""""
-    for i in range(1, 100):
+    for i in range(1, 101):
         print(i)
+        
 print('Les poires sont meilleures que les pommes')
 print('On dit chocolatine')
+
 
