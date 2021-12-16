edc0914c7107b6d9de161d70d84abc497beda457
Adri-78 <91499346+Adri-78@users.noreply.github.com>
2021-12-16 11:33:44 +0100

    Update compteur.py

--- compteur.py
+++ compteur.py
@@ -1,6 +1,6 @@
 def compte():
     r"""Compte de 1 à 100""""
-    for i in range(1, 100):
+    for i in range(1, 101):
         print(i)
 
-# cela apparaît bien faux
+# cela apparaît bien faux, il faut remplacer 100 par 101
